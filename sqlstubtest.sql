b65b342135d194a36ed38306aa970d6a03314378
Fabrizio - FDS Development <58892804+fabryyzzz@users.noreply.github.com>
2024-06-19 15:01:30 +0200

    Update sqlstubtest.sql

--- sqlstubtest.sql
+++ sqlstubtest.sql
@@ -1,25 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versione server:              10.4.24-MariaDB - mariadb.org binary distribution
--- S.O. server:                  Win64
--- HeidiSQL Versione:            12.0.0.6468
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Dump della struttura del database essentialmode
-CREATE DATABASE IF NOT EXISTS `essentialmode` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `essentialmode`;
-
--- Dump della struttura di tabella essentialmode.fdsdev_stubtest
 CREATE TABLE IF NOT EXISTS `fdsdev_stubtest` (
   `date` varchar(50) DEFAULT NULL,
   `player` varchar(50) DEFAULT NULL,
@@ -31,10 +9,8 @@ CREATE TABLE IF NOT EXISTS `fdsdev_stubtest` (
   KEY `Indice 2` (`player`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dump dei dati della tabella essentialmode.fdsdev_stubtest: ~0 rows (circa)
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+CREATE TABLE IF NOT EXISTS `fdsdev_gunpowderdata` (
+  `identifier` varchar(60) DEFAULT NULL,
+  `stato` varchar(50) DEFAULT NULL,
+  `shootday` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
